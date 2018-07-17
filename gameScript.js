203441fe6f6c7d1298bb9473303e56c8ac2f5d1e
Trenton Daniels <trentondaniels95@gmail.com>
2018-07-17 14:02:36 -0500

    ammended index.html to contain the total and a description upon results

--- gameScript.js
+++ gameScript.js
@@ -6,31 +6,31 @@ function calculateHotdogsEaten () {
 
 function calculateHamburgersEaten () {
     let result = Math.round(Math.random() * 6);
-    document.getElementById('hamburger-num').innerHTML = '<p>' + result + '</p>';
+    document.getElementById('hamburger-num').innerHTML = `<p>${result}</p>`;
     return result;
 }
 
 function calculateBigMacsEaten () {
     let result = Math.round(Math.random() * 8); 
-    document.getElementById('bigmac-num').innerHTML = '<p>' + result + '</p>';
+    document.getElementById('bigmac-num').innerHTML = `<p>${result}</p>`;
     return result;
 }
 
 function calculateFriesEaten () {
     let result = Math.round(Math.random() * 10);
-    document.getElementById('fries-num').innerHTML = '<p>' + result + '</p>';
+    document.getElementById('fries-num').innerHTML = `<p>${result}</p>`;
     return result;
 }
 
 function calculateBeersDrinken () {
     let result = Math.round(Math.random() * 12);
-    document.getElementById('beer-num').innerHTML = '<p>' + result + '</p>';
+    document.getElementById('beer-num').innerHTML = `<p>${result}</p>`;
     return result;
 }
 
 function calculateNuggetsEaten () {
     let result = Math.round(Math.random() * 20);
-    document.getElementById('nugget-num').innerHTML = '<p>' + result + '</p>';
+    document.getElementById('nugget-num').innerHTML = `<p>${result}</p>`;
     return result;
 }
 
@@ -56,10 +56,10 @@ function handleFood() {
 handleFood();
 
 function isAlive(total) {
-    if (total > 40) {
+    if (total > 35) {
         return 'Congratulations! You ate all of the food and lived to tell the tale easily!'
     }
-    else if (total >= 30 && total <=40) {
+    else if (total >= 25 && total <=35) {
         return 'Wow, you barely made it. Try not to eat so much next time!'
     }
     else {
